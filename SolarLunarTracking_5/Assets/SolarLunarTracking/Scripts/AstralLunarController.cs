5731eb37306895531c00e554d4dc55c8f8b6dcbf
Roger Lew <rogerlew@gmail.com>
2015-03-31 16:26:25 -0700

    Updated for Unity 5

---
+++ SolarLunarTracking_5/Assets/SolarLunarTracking/Scripts/AstralLunarController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AstralLunarController : MonoBehaviour
+{
+    private float latitude = 43f;
+    private float longitude = -116f;
+    private float altitude = 1160;
+    private float timeZone = -7;
+
+    private Light moon;
+    private GameObject plane;
+    private List<Texture> moonTextures;
+    private int currentTex = 11;
+
+    private GameObject slider;
+    private TimeSlider TimeSliderScript;
+
+    private Astral astral;
+    private Lunar lunar;
+    public Lunar lunarProperty
+    {
+        get { return lunar; }
+        set { lunar = value; }
+    }
+
+    void Start()
+    {
+        // Read the geolocation from the parent
+        GameObject parent = transform.parent.gameObject;
+        SolarLunarTrackingGeolocation geoLoc = parent.GetComponent<SolarLunarTrackingGeolocation>();
+        latitude = geoLoc.latitude;
+        longitude = geoLoc.longitude;
+        altitude = geoLoc.altitude;
+        timeZone = geoLoc.timeZone;
+
+#if DEBUG
+        Debug.Log(String.Format("Location: {0}, {1}", longitude, latitude));
+        Debug.Log(String.Format("Altitude: {0}", altitude));
+        Debug.Log(String.Format("Time Zone: {0}", timeZone));
+#endif
+
+        slider = GameObject.FindWithTag("TimeSlider");
+        TimeSliderScript = (TimeSlider)slider.GetComponent(typeof(TimeSlider));
+
+        astral = new Astral(longitude, latitude, altitude, timeZone);
+        moon = transform.Find("Plane").gameObject.GetComponent<Light>();
+        plane = transform.Find("Plane").gameObject;
+
+        moonTextures = new List<Texture>();
+        for (int i =0; i<28; i++)
+        {
+            string fname = string.Format("Assets/SolarLunarTracking/moonTextures/Moon{0}.png", i.ToString("D2"));
+            moonTextures.Add(Resources.LoadAssetAtPath<Texture>(fname));
+        }
+    }
+
+
+
+    void Update()
+    {
+        DateTime datetime = TimeSliderScript.SimTime;
+        
+        lunar = astral.lunar(datetime);
+        transform.eulerAngles = new Vector3(270 + (float)lunar.elevation, 180 + (float)lunar.azimuth, 0);
+
+        int matInd = Mathf.FloorToInt((float)lunar.phase * 28f);
+
+        if (lunar.elevation < -20)
+        {
+            if (currentTex != matInd)
+            {
+                plane.GetComponent<Renderer>().material.mainTexture = moonTextures[matInd];
+                currentTex = matInd;
+            }
+        }
+
+    }
+}
---
+++ SolarLunarTracking_5/Assets/SolarLunarTracking/Scripts/PlaneBillboarder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneBillboarder : MonoBehaviour {
+
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.renderQueue = 3000;
+    }
+
+	// Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
+        transform.Rotate(new Vector3(90, 0, 0));
+	}
+}
